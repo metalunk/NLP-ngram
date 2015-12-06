4f87c119711f9d09d7e99c91090206a40a9207b2
metalunk <chiba.r221@gmail.com>
2015-12-06 20:10:00 +0900

    Make trigram model

--- ngram/bigram.rb
+++ ngram/bigram.rb
@@ -210,7 +210,7 @@ class Trigram < Ngram
     trigram_model, sum_p = kneser_ney
     trigram_model = good_turing_distribution trigram_model, sum_p
 
-    this.output_to_file trigram_model, "#{OUTPUT_FILE_PREFIX}#{@target_words[0]}_#{@target_words[1]}"
+    Ngram::output_to_file trigram_model, "#{OUTPUT_FILE_PREFIX}#{@target_words[0]}_#{@target_words[1]}"
   end
 
   private
@@ -220,14 +220,60 @@ class Trigram < Ngram
   def kneser_ney
     count_by_word_after_targets = @file_reader.count_by_word_after_targets(@target_words)
     count_all_word_after_targets = count_all_word_after_targets count_by_word_after_targets
+    count_by_word = @file_reader.count_by_word
+
+    trigram_model = {}
+    pre_sum_p = 0.0
+    count_by_word.each do |word,|
+      p = [count_by_word_after_targets[word].to_f - CONST_D, 0.0].max / count_all_word_after_targets.to_f
+      trigram_model[word] = p
+      pre_sum_p += p
+    end
+
+    bigram_model = bigram_model()
+
+    sum_p = 0.0
+    trigram_model.each do |word, p|
+      trigram_model[word] = p + (1.0 - pre_sum_p) * bigram_model[word]
+      sum_p += trigram_model[word]
+    end
+
+    [trigram_model, sum_p]
   end
 
-  def good_turing_distribution (discounted_bigram, sum_p)
+  def bigram_model
+    bigram_file_name = Bigram::OUTPUT_FILE_PREFIX + @target_words[1].to_s
+
+    if !File.exists? bigram_file_name
+      bigram = Bigram.new @file_reader, [@target_words[1]]
+      bigram.calc
+    end
+
+    i = 0
+    bigram_model = {}
+    File.open(bigram_file_name, 'r') do |file|
+      file.each_line do |line|
+        bigram_model[i] = line.to_f
+        i += 1
+      end
+    end
+
+    bigram_model
+  end
+
+  def good_turing_distribution (trigram_model, sum_p)
+    count_by_word = @file_reader.count_by_word
+    count_all_word = @file_reader.count_all_word
+
+    trigram_model.each do |word, p|
+      trigram_model[word] = p + (1.0 - sum_p) * count_by_word[word].to_f / count_all_word.to_f
+    end
+    trigram_model
   end
 
   def count_all_word_after_targets (count_by_word_after_targets)
     sum_count = 0
-    count_by_word_after_targets.each do |count|
+    count_by_word_after_targets.each do |word, count|
       sum_count += count
     end
     sum_count
